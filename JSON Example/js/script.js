06ec7a5dc97d21caea74faf2b5938593beda1807
Karl George <kelsyerg@gmail.com>
2016-11-26 13:28:55 +0000

    Changed name dir

--- JSON Example/js/script.js
+++ JSON Example/js/script.js
@@ -7,7 +7,7 @@ document.querySelector("button").addEventListener("click", function() {
     var name = "";
 
     // Call Server to get the name
-    $ajaxUtils.sendGetRequest("/data/name.txt", function (request) {
+    $ajaxUtils.sendGetRequest("https://github.com/Remdran/RandomProjects/tree/gh-pages/JSON%20Example/data/name.txt", function (request) {
         self.name = request.responstText;
 
         document.querySelector("#content").innerHTML = "<h2>Hello " + self.name + "!</h2>";
