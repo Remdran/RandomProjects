d57d30ce14a29bfe13fd20a1910cbd784b6edfdd
Karl George <kelsyerg@gmail.com>
2016-11-26 13:32:43 +0000

    +2

--- JSON Example/js/script.js
+++ JSON Example/js/script.js
@@ -7,7 +7,7 @@ document.querySelector("button").addEventListener("click", function() {
     var name = "";
 
     // Call Server to get the name
-    $ajaxUtils.sendGetRequest("https://raw.githubusercontent.com/Remdran/RandomProjects/gh-pages/JSON%20Example/data/name.txt", function (request) {
+    $ajaxUtils.sendGetRequest("https://remdran.github.io/RandomProjects/JSON%20Example/data/name.txt", function (request) {
         self.name = request.responstText;
 
         document.querySelector("#content").innerHTML = "<h2>Hello " + self.name + "!</h2>";
