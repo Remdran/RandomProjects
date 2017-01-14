ca9a38e6efa2f278c23056d39a7ee9dc0cd3c1c2
Karl George <kelsyerg@gmail.com>
2017-01-14 13:06:14 +0000

    Worked on Interpreter
    -Need to redesign the buttons so theyre easier to style etc
    -Make the JS execution secure

--- Projects/Code_Interpretor/js/script.js
+++ Projects/Code_Interpretor/js/script.js
@@ -1,27 +1,31 @@
- $( '.controlgroup' ).controlgroup()
+var active = 4 - $( '.hide' ).length;
+$('.controlgroup').controlgroup()
 
- $( '#html' ).click(function(){
-     $( '#htmlWindow' ).toggle();
- });
+$('.windows').height($(window).height() - $('#header').height() - 20);   // Set the individual windows height equal to the height of the window - header - padding
+$('.windows').width($(window).width() / active - 3); // Set the individual windows width equal to the width of the window  / number of panels - padding
+//console.log($(window).width());
+//console.log($('.windows').width());
 
-  $( '#css' ).click(function(){
-      $( '#cssWindow' ).toggle();
-  });
+updateOutput();
 
-  $( '#js' ).click(function(){
-      $( '#jsWindow' ).toggle();
- });
+$('.controlButton').click(function () {
+    $('#' + this.dataset.name + "Content").toggleClass("hide");
 
-  $( '#output' ).click(function(){
-      $( '#outputWindow' ).toggle();
-  });
+    active = 4 - $( '.hide' ).length;
+    $('.windows').width(($(window).width() / active) - 3);
+    //console.log($(window).width());
+    //console.log($('.windows').width());
+});
 
-  $( '#htmlContent' ).keyup(function(){
-      var htmlStr = $( '#htmlContent' ).val();
-      $( '#outputWindow' ).html(htmlStr);
-  });
-  
-    $( '#cssContent' ).keyup(function(){
-      var cssStr = $( '#cssContent' ).val();
-      $( '#outputWindow' ).css(cssStr);
-  });
+$('textarea').on('change keyup paste', function () {
+   updateOutput();
+});
+
+function updateOutput() {
+    $('iframe').contents().find('html').html("<html><head><style type='text/css'>" + $('#cssContent').val() + "</style></head></body>" + $('#htmlContent').val() + "</body></html>")
+    ;
+
+    // Disable javascript execution whilst its unsecure for now
+    //document.getElementById('outputContent').contentWindow.eval($( '#jsContent').val());
+}
+
