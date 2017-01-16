5cd39f52a50fbb07e0448599289d4ed2622a2740
Karl George <kelsyerg@gmail.com>
2017-01-16 13:42:42 +0000

    Weather App functionality complete
    Completed weather app functionality, just need to refactor code to tidy
    it up

--- Projects/Local_Weather_App/js/script.js
+++ Projects/Local_Weather_App/js/script.js
@@ -1,13 +1,16 @@
-var icons = {   
-    "sunny" : "http://icons.iconarchive.com/icons/custom-icon-design/weather/256/Sunny-icon.png",
-    "sunCloud" : "http://icons.iconarchive.com/icons/custom-icon-design/weather/256/Sunny-Period-icon.png",
-    "cloud" : "http://icons.iconarchive.com/icons/custom-icon-design/weather/256/Overcast-icon.png",
-    "rain" : "http://icons.iconarchive.com/icons/custom-icon-design/weather/128/light-rain-icon.png",
-    "heavyRain" : "http://icons.iconarchive.com/icons/custom-icon-design/weather/256/rain-icon.png",
-    "snow" : "http://icons.iconarchive.com/icons/custom-icon-design/weather/256/snow-icon.png",
-    "thunder" : "http://icons.iconarchive.com/icons/custom-icon-design/weather/256/thunder-icon.png",
-    "windy" : "http://icons.iconarchive.com/icons/iconsmind/outline/256/Windy-icon.png"
+var icons = {
+    "sunny": "http://icons.iconarchive.com/icons/custom-icon-design/weather/256/Sunny-icon.png",
+    "sunCloud": "http://icons.iconarchive.com/icons/custom-icon-design/weather/256/Sunny-Period-icon.png",
+    "cloud": "http://icons.iconarchive.com/icons/custom-icon-design/weather/256/Overcast-icon.png",
+    "rain": "http://icons.iconarchive.com/icons/custom-icon-design/weather/128/light-rain-icon.png",
+    "heavyRain": "http://icons.iconarchive.com/icons/custom-icon-design/weather/256/rain-icon.png",
+    "snow": "http://icons.iconarchive.com/icons/custom-icon-design/weather/256/snow-icon.png",
+    "thunder": "http://icons.iconarchive.com/icons/custom-icon-design/weather/256/thunder-icon.png",
+    "windy": "http://icons.iconarchive.com/icons/iconsmind/outline/256/Windy-icon.png"
 }
+var showingCel = true;
+var fahr;
+var temp;
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -20,11 +23,13 @@ if (navigator.geolocation) {
             data: {},
             dataType: 'json',
             success: function (data) {
-                console.log(data);
-                $( '#location' ).html(data.query.results.channel.location.city + "<br>" + data.query.results.channel.location.country);
+                //console.log(data);
+                $('#location').html(data.query.results.channel.location.city + ", " + data.query.results.channel.location.region);
                 // Store the weather condition in a variable and pass it to a function to set the icon
-                var weather = data.query.results.channel.item.condition.text;
-                setupIcon(weather);                
+                var weatherIcon = data.query.results.channel.item.condition.text;
+                setupWeather(weatherIcon);
+                var temperature = data.query.results.channel.item.condition.temp;
+                setupTemp(temperature);
             },
             error: function (err) { alert(err); },
             beforeSend: function (xhr) {
@@ -37,24 +42,45 @@ else {
 
 }
 
-function setupIcon(weather) {
-    console.log(weather);
+function setupWeather(weather) {
+    //console.log(weather);
 
     if (weather.includes("Sun")) {
-         $( '#icon' ).attr("src", icons.sunny);        
+        $(' #icon ').attr("src", icons.sunny);
+        $( '#weatherInfo').html(weather);
     } else if (weather.includes("Breezy") || weather.includes("Wind")) {
-         $( '#icon' ).attr("src", icons.windy);
+        $(' #icon ').attr("src", icons.windy);
+        $( '#weatherInfo').html(weather);
     } else if (weather.includes("Cloud")) {
-        $( '#icon' ).attr("src", icons.cloud);
+        $(' #icon ').attr("src", icons.cloud);
+        $( '#weatherInfo').html(weather);
     } else if (weather.includes("Rain") || weather.includes("Shower")) {
-        $( '#icon' ).attr("src", icons.heavyRain);
+        $(' #icon ').attr("src", icons.heavyRain);
+        $( '#weatherInfo').html(weather);
     } else if (weather.includes("Snow")) {
-        $( '#icon' ).attr("src", icons.snow);
+        $(' #icon ').attr("src", icons.snow);
+        $( '#weatherInfo').html(weather);
     } else {
-         $( '#icon' ).attr("src", icons.sunny);
+        $(' #icon ').attr("src", icons.sunny);
+        $( '#weatherInfo').html(weather);
     }
 }
 
+function setupTemp(temper) {
+    //console.log(temper);
+    temp = temper;
+    $( '#temp' ).html(temp);
+    fahr = (temp * 9 / 5) + 32;
+}
 
-
-
+$( '.deg' ).click(function() {
+    if(showingCel === true) {
+        $( '#temp' ).html(fahr);
+        $( '.deg' ).html("&#8457;");
+        showingCel = false;
+    } else {
+        $( '#temp' ).html(temp);
+        $( '.deg' ).html("&#8451;");
+        showingCel = true;
+    }    
+});
